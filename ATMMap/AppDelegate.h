8fd9c3f46884afbb035cd90332f70d698df07029
aiuar <aivar_xid@mail.ru>
2017-05-11 13:11:11 +0300

    Initial Commit

---
+++ ATMMap/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ATMMap
+//
+//  Created by aiuar on 11.05.17.
+//  Copyright © 2017 A.V. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ATMMap/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ATMMap
+//
+//  Created by aiuar on 11.05.17.
+//  Copyright © 2017 A.V. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
