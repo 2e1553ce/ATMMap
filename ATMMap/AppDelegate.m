a8195d2ad6c323e5d8922900f1be401e287050a6
aiuar <aivar_xid@mail.ru>
2017-05-19 20:20:46 +0300

    Multi delegate

--- ATMMap/AppDelegate.m
+++ ATMMap/AppDelegate.m
@@ -7,9 +7,7 @@
 //
 
 #import "AppDelegate.h"
-#import "AVGATMTableViewController.h"
-#import "AVGMapViewController.h"
-#import "AVGATMService.h"
+#import "AVGTabBarController.h"
 
 @interface AppDelegate ()
 
@@ -22,35 +20,8 @@
     
     UIWindow *window = [[UIWindow alloc] initWithFrame:UIScreen.mainScreen.bounds];
     
-    UITabBarController *tabBarController = [UITabBarController new];
-    
-    // Navigation controllers
-    UINavigationController *mapNavigationController = [UINavigationController new];
-    mapNavigationController.tabBarItem.title = @"Карта";
-    mapNavigationController.tabBarItem.titlePositionAdjustment = UIOffsetMake(0.f, -10.f);
-    
-    UINavigationController *tableNavigationController = [UINavigationController new];
-    tableNavigationController.tabBarItem.title = @"Список";
-    tableNavigationController.tabBarItem.titlePositionAdjustment = UIOffsetMake(0.f, -10.f);
-    
-    // Service
-    AVGATMService *service = [AVGATMService new];
-    
-    // Map view controller
-    AVGMapViewController *mapViewController = [AVGMapViewController new];
-    mapViewController.atmService = service;
-    mapNavigationController.viewControllers = @[mapViewController];
-    
-    [mapViewController.tabBarItem setTitlePositionAdjustment:UIOffsetMake(-100, -100)];
-    
-    // Table view controller
-    AVGATMTableViewController *atmTableViewController = [AVGATMTableViewController new];
-    atmTableViewController.atmService = service;
-    tableNavigationController.viewControllers = @[atmTableViewController];
-    
-    // Setting Tabbar controller
-    tabBarController.viewControllers = @[mapNavigationController, tableNavigationController];
-    
+    AVGTabBarController *tabBarController = [AVGTabBarController new];
+
     window.rootViewController = tabBarController;
     self.window = window;
     [window makeKeyAndVisible];
